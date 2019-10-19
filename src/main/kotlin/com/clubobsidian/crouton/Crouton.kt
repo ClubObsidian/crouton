c5d1d6e0094ec6674766c975dd622a9fec3e5005
virustotalop <virustotalop@gmail.com>
2019-10-19 16:37:06 -0700

    Make bracket style consistent
    Kotlin requires K&R style brackets for async so the code should adheer to K&R style brackets throughout.

--- src/main/kotlin/com/clubobsidian/crouton/Crouton.kt
+++ src/main/kotlin/com/clubobsidian/crouton/Crouton.kt
@@ -28,12 +28,9 @@ class Crouton public constructor() {
     private var job: Job? = null;
     private val running = AtomicBoolean(true);
 
-    fun createAsyncJob(runnable : Runnable) : Job
-    {
-        if(this.job == null)
-        {
-            val newJob = GlobalScope.launch()
-            {
+    fun createAsyncJob(runnable : Runnable) : Job {
+        if(this.job == null) {
+            val newJob = GlobalScope.launch() {
                 async {
                     runnable.run();
                 }
@@ -44,12 +41,9 @@ class Crouton public constructor() {
         return this.job!!;
     }
 
-    fun createAsyncRepeatingJob(runnable: Runnable, initialDelay : Long, repeatingDelay : Long) : Job
-    {
-        if(this.job == null)
-        {
-            val newJob = GlobalScope.launch()
-            {
+    fun createAsyncRepeatingJob(runnable: Runnable, initialDelay : Long, repeatingDelay : Long) : Job {
+        if(this.job == null) {
+            val newJob = GlobalScope.launch() {
                 async {
                     delay(initialDelay);
                     while (isRunning().get()) {
@@ -64,13 +58,10 @@ class Crouton public constructor() {
         return this.job!!;
     }
 
-    fun createAsyncWait(future: Future<Any>) : Future<Any>
-    {
+    fun createAsyncWait(future: Future<Any>) : Future<Any> {
         val completedFuture = CompletableFuture<Any>();
-        if(this.job == null)
-        {
-            val newJob = GlobalScope.launch()
-            {
+        if(this.job == null) {
+            val newJob = GlobalScope.launch() {
                 async {
                     completedFuture.complete(future.get());
                 }
@@ -80,23 +71,19 @@ class Crouton public constructor() {
         return future;
     }
 
-    suspend fun delayJob(millis: Long)
-    {
+    suspend fun delayJob(millis: Long) {
         delay(millis);
     }
 
-    fun isRunning() : AtomicBoolean
-    {
+    fun isRunning() : AtomicBoolean {
         return this.running;
     }
 
-    fun getJob() : Job
-    {
+    fun getJob() : Job {
         return this.job!!;
     }
 
-    fun stop()
-    {
+    fun stop() {
         this.running.set(false);
     }
 }
