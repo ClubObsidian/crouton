641ccc1146d0ad9fc93ff0d7f56806c0934f4960
virustotalop <virustotalop@gmail.com>
2019-10-19 22:52:41 -0700

    Reorder job wrapper methods

--- src/main/kotlin/com/clubobsidian/crouton/wrapper/JobWrapper.kt
+++ src/main/kotlin/com/clubobsidian/crouton/wrapper/JobWrapper.kt
@@ -9,15 +9,15 @@ open class JobWrapper public constructor() {
     private val running = AtomicBoolean(true);
     private var job: Job? = null;
 
+    fun getJob() : Job? {
+        return this.job;
+    }
+
     fun setJob(job : Job?) {
         this.job = job;
     }
 
     fun isRunning() : AtomicBoolean {
         return this.running;
-    }
-
-    fun getJob() : Job? {
-        return this.job;
     }
 }
